d8194676ad6d0c5f8572480bc32ef33d5accc32c
Berkin <103454964+berkinkinay@users.noreply.github.com>
2023-03-13 14:04:18 +0300

    Update Sidebar.jsx

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,7 +5,6 @@ import * as React from "react";
 import { NavLink } from 'react-router-dom';
 import AppLogo from './AppLogo';
 
-
 export default function Sidebar () {
   return (
     <div className="flex flex-col gap-10 h-fit w-fit items-center justify-center cursor-default">
