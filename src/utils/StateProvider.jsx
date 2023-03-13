3eb0829f20a575205442e6f1a2b81dbf9180b6c6
Berkin <103454964+berkinkinay@users.noreply.github.com>
2023-03-13 13:40:47 +0300

    Update -v2.1

--- src/utils/StateProvider.jsx
+++ src/utils/StateProvider.jsx
@@ -4,6 +4,7 @@ export const StateContext = createContext();
 
 export const StateProvider = ({ initialState, reducer, children }) => (
   <StateContext.Provider value={useReducer(reducer, initialState)}>
+  
     {children}
   </StateContext.Provider>
 );
